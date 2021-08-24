adb15b9b91aae528ac1b5c81d2d7e22b9b419b13
Alexey Sosnin <sosnin.alexey@outlook.com>
2021-08-24 21:44:24 +0300

    feat: implement first path of desktop UI

---
+++ src/Heartbeat.Hosting.Desktop/Views/ObjectTypeStatisticsView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Heartbeat.Hosting.Desktop.Views
+{
+    public class ObjectTypeStatisticsView : UserControl
+    {
+        public ObjectTypeStatisticsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
