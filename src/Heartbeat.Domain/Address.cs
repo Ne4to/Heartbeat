234f1c3510c5df0bceb585f59209c4ab628ed925
Alexey Sosnin <sosnin.alexey@outlook.com>
2022-08-11 02:50:13 +0300

    add object instance page

--- src/Heartbeat.Domain/Address.cs
+++ src/Heartbeat.Domain/Address.cs
@@ -8,4 +8,6 @@ public readonly record struct Address(ulong Value)
     {
         return $"0x{Value:x}";
     }
+
+    public static implicit operator ulong(Address address) => address.Value;
 }
---
+++ src/Heartbeat.Web/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace Heartbeat.Web;
+
+internal static class Extensions
+{
+    public static ulong ParseHex(this string value) => 
+        ulong.Parse(value, NumberStyles.HexNumber);
+}
